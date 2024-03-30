804151f012b596037af301cd278dabc650cf50a6
AlvaroG <ninorokr2@gmail.com>
2024-03-30 13:03:26 -0500

    Primer commit, primeras ideas

---
+++ scripts.js
@@ -0,0 +1,5 @@
+//CARRITO DE COMPRAS
+//1. Dar al usuario la opción de elegir articulos de un listado
+//Cada uno tiene su precio y se puede indicar la cantidad a llevar
+//2. Guardar las elecciones en una cadena (string) y acumular el monto total en una variable (float)
+
